2b2d38f0f54ca0e9798cfa9850eca3a1cd012931
naaddanda <112264617+naaddanda@users.noreply.github.com>
2026-05-07 14:51:10 +0300

    Админ

---
+++ SalonAdmin/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SalonAdmin
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void Nav_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.Tag is string tag)
+            {
+                var type = Type.GetType($"SalonAdmin.Pages.{tag}Page");
+                if (type != null) MainFrame.Navigate((Page)Activator.CreateInstance(type));
+            }
+        }
+    }
+}
